e147c5bcba4c4d5f1e2396a1eab5199433124e31
Blazej Duszynski <bduszynski92@gmail.com>
2023-07-20 18:53:10 +0200

    added first animations in GSAP

--- js/script.js
+++ js/script.js
@@ -1,17 +1,23 @@
 // fade in offer items start
 const offerFadeInItems = document.querySelectorAll(".offer__item");
 
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        entry.target.classList.toggle("offer__item--transitioned", entry.isIntersecting)
-        if (entry.isIntersecting) observer.unobserve(entry.target)
-    })
-}, {
-    threshold: 0.5
-})
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      entry.target.classList.toggle(
+        "offer__item--transitioned",
+        entry.isIntersecting
+      );
+      if (entry.isIntersecting) observer.unobserve(entry.target);
+    });
+  },
+  {
+    threshold: 0.5,
+  }
+);
 
-offerFadeInItems.forEach(offerFadeInItem => {
-    observer.observe(offerFadeInItem);
+offerFadeInItems.forEach((offerFadeInItem) => {
+  observer.observe(offerFadeInItem);
 });
 
 //fade in offer items end
@@ -21,8 +27,20 @@ const hamburger = document.querySelector(".header__burgerMenu");
 const navMenu = document.querySelector(".header__navList");
 
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 });
 
 // burger menu activation end
+gsap.from(".header", {
+  duration: 0.8,
+  y: "-150%",
+  delay: 1.5,
+});
+
+gsap.from(".hero__container", {
+    duration: 1,
+    opacity: 0,
+    delay: 1,
+    stagger: 0.2
+  });
