2d390bf49c45742f045c5cbd246f578ab8c4a6fd
Blazej Duszynski <bduszynski92@gmail.com>
2023-08-18 12:32:34 +0200

    added close popup functionality

--- js/gallery.js
+++ js/gallery.js
@@ -1,8 +1,13 @@
 const thumbnails = document.querySelectorAll(".gallery__thumbnail img");
 const popup = document.querySelector(".popup");
+const popupClose = document.querySelector(".popup__close");
 
 thumbnails.forEach((thumbnail) => {
   thumbnail.addEventListener("click", () => {
     popup.classList.remove("hidden");
   });
+});
+
+popupClose.addEventListener("click", () => {
+  popup.classList.add("hidden");
 });
