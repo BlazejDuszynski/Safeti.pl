bc1258a653aba55b8c958d504c9788234a8b3fc5
Blazej Duszynski <bduszynski92@gmail.com>
2023-08-18 12:37:56 +0200

    injected src of thumbnail to popup image src

--- js/gallery.js
+++ js/gallery.js
@@ -1,10 +1,13 @@
 const thumbnails = document.querySelectorAll(".gallery__thumbnail img");
 const popup = document.querySelector(".popup");
+const popupImg = document.querySelector(".popup__img");
 const popupClose = document.querySelector(".popup__close");
 
 thumbnails.forEach((thumbnail) => {
   thumbnail.addEventListener("click", () => {
     popup.classList.remove("hidden");
+    popupImg.src = thumbnail.src;
+    popupImg.alt = thumbnail.alt;
   });
 });
 
